61baf5fa60e764193ec62b835e3d6018bd7cb516
Mounir El-Abbas <eamounir1@gmail.com>
2021-08-21 14:28:59 -0400

    Basic JS

--- tttScript.js
+++ tttScript.js
@@ -1,1 +1,23 @@
+const gameBoard = document.querySelector('.game-container')
+window.addEventListener('DOMContentLoaded', setup)
 
+function setup(){
+  gameBoard.innerHTML = `
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  `
+}
+
+function boardSpotChosen(){
+  for (let eachSpot of gameBoard.children){
+    console.log(eachSpot)
+  }
+}
+boardSpotChosen()
